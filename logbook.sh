4767dbc479f3a64ccaa5615cd8eae6d7f195f8f3
marky <markygriff12@gmail.com>
2017-12-04 20:45:11 +0000

    test

--- logbook.sh
+++ logbook.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 
-function lb() {
-  file = ${date '+%Y-%m-%d).md
-  flag = 0;
+function lb () {
+  file=$(date '+%Y-%m-%d').md
+  flag=0;
   
   cd ~/Desktop/LogBook
   
   vim ~/Desktop/LogBook/$file
-  [? -eq 0] || (echo vim failure && return);
+
+  [ $? -eq 0 ] || (echo vim failure && return);
   
   git add ~/Desktop/LogBook/$file
-  [? -eq 0] || (echo "add failed $(?)" %% return);
+  [ $? -eq 0 ] || (echo "add failed $(?)" && return);
   
-  while [ $flag -eq 0]; do
+  while [ $flag -eq 0 ]; do
     git commit -m "update $(date '+%c')";
     if [ $? -eq 0 ]; then flag=1;
     else
       while true; do
-        echo -e "\n"
-        read -p "Commit failed. Please connect to 'Actual Internet' press enter when connected to q to quite: " resp;
+        echo -e "\nCommit failed. If you are not connected to 'Actual Internet', please connect and press enter.\n"
+
+        read -p "Or you can exit, by typing q: " resp;
         case $resp in 
           [qQ] ) return;;
           * ) break;;
         esac
       done
-    fl;
+    fi;
   done
+  git pull
   git push origin master;
+  }
